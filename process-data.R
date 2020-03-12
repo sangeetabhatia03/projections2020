6d958f9f71fda2321a0fec2c758aef562725fffa
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2020-03-12 18:20:50 +0000

    negbin + more time windows

--- process-data.R
+++ process-data.R
@@ -53,11 +53,13 @@ SI_Distr <- sapply(
 )
 
 SI_Distr <- SI_Distr / sum(SI_Distr)
+
+size <- 0.16
 ## # serial interval estimate used: mean = 3.96, sd =  4.75
 ## # from Du et al. from The University of Texas at Austin and University of Hong Kong
 ## # (https://www.medrxiv.org/content/10.1101/2020.01.28.20019299v4.full.pdf)
 ## # (https://www.medrxiv.org/content/10.1101/2020.02.19.20025452v2.full.pdf)
-time_window <- c(4, 8)
+time_window <- c(2, 4, 6, 8)
 names(time_window) <- time_window
 
 r0s <- purrr::map_dfr(
@@ -90,6 +92,59 @@ r0s <- purrr::map_dfr(
 )
 
 ####
+#### Uncertain SI #########
+##### Crude way of doing this for now ################
+##### Look at various estimates of SI and get mean, and s.d. of mean
+#####
+## mean_estimates <- c(
+##     7.5, 4.41, 4.0, 4.6, 3.96, 5.1, 3.3, 6.3, 4.56, 4.22, 6.37, 5.9, 5.2, 3.95
+## )
+## sd_mean <- sd(mean_estimates)
+## sd_estimates <- c(
+##     3.17, 4.75, 4.8
+## )
+## sd_sd <- sd(sd_estimates)
+
+## r0s <- purrr::map_dfr(
+##     incidence,
+##     function(df) {
+##         out <- map_dfr(
+##             time_window,
+##             function(tw) {
+##                 I <- df$cases
+##                 start <- 2:(nrow(df) - tw)
+##                 end <- start + tw
+##                 res <- EpiEstim::estimate_R(
+##                                      I,
+##                                      #T.Start = start,
+##                                      #T.End = end,
+##                                      method = "uncertain_si",
+##                                      config = EpiEstim::make_config(
+##                                          list(
+##                                              t_start = start,
+##                                              t_end = end,
+##                                              mean_si = mean(mean_estimates),
+##                                              std_mean_si = sd_mean,
+##                                              min_mean_si = 1,
+##                                              max_mean_si = 8,
+##                                              std_si = mean(sd_estimates),
+##                                              std_std_si = sd_sd,
+##                                              min_std_si = 1,
+##                                              max_std_si = 5,
+##                                              n1 = 100,
+##                                              n2 = 100
+##                                          )
+##                                      )
+##                                  )
+##                 res$R$date <- df$date[end]
+##                 res$R
+##             },
+##           .id = "time_window"
+##         )
+
+##     }, .id = "country"
+## )
+
 
 ggplot() +
     geom_line(
@@ -111,10 +166,7 @@ ggplot() +
 
 
 nsim <- 1000
-
-custom_join <- function(sim1, sim2) {
-    dplyr::left_join(sim1, sim2, by = "date")
-}
+n_days <- 21
 
 r0s <- split(r0s, r0s$country)
 date_to_project_from <- "2020-03-03"
@@ -135,34 +187,16 @@ projections <- purrr::imap(
         incid <- map_dfr(
             country_r0_tw,
             function(r0) {
-                incid <- map(
-                    1:nsim,
-                    function(i) {
-                        incid <- project(
-                            incid = matrix(I, ncol = 1),
-                            R = matrix(r0[["Median(R)"]][nrow(r0)], ncol = 1),
-                            si = SI_Distr,
-                            pij = matrix(1, nrow = 1, ncol = 1),
-                            n_days = 21
-                        )
-                        out <- data.frame(
-                            date = seq(
-                                from = as.Date(date_to_project_from),
-                                length.out = 21,
-                                by = "1 day"
-                            ),
-                            incid = incid
-                        )
-                        out
-                    }
-
+                R <- r0[["Median(R)"]][nrow(r0)]
+                pij <- matrix(1, nrow = 1, ncol = 1)
+                incid <- project_sims(
+                    nsim, I, R, si, pij, size, n_days, date_to_project_from
                 )
-                incid <- Reduce(f = custom_join, x = incid)
                 incid
         },
         .id = "time_window"
    )
-    }
+  }
  )
 
 quantiles <- purrr::map(
@@ -218,3 +252,56 @@ for (country in names(incidence)) {
 
 
 
+####### rmse
+
+rmse <- map2(
+    incidence,
+    projections,
+    function(obs, pred) {
+        x <- tail(obs$cases, n_days)
+        tw_y <- split(pred, pred$time_window)
+        rel_mse <- map_dfr(
+            tw_y,
+            function(pred2) {
+                pred2 <- as.matrix(pred2[ , -c(1, 2)])
+                out <- assessr::rel_mse(x, pred2)
+                out <- data.frame(
+                    date = tail(obs$date, n_days),
+                    relmse = out
+                )
+                out$date <- as.Date(out$date)
+                out
+            },
+            .id = "time_window"
+        )
+        rel_mse
+
+    }
+)
+
+
+for (country in names(incidence)) {
+
+    p <- ggplot() +
+        geom_line(
+            data = rmse[[country]],
+            aes(date, relmse, col = time_window)
+        ) +
+        theme_classic() +
+        theme(legend.position = "bottom") +
+        geom_vline(
+            xintercept = as.numeric(as.Date(date_to_project_from)),
+            linetype = 4
+        )
+    plog <- p +
+        scale_y_log10(
+            breaks = scales::trans_breaks("log10", function(x) 10^x),
+            labels = scales::trans_format("log10", scales::math_format(10^.x))
+        ) +
+        annotation_logticks(sides = "l")
+
+    outfile <- snakecase::to_snake_case(country)
+    ggsave(glue::glue("{outfile}_rmse.pdf"), p)
+    ggsave(glue::glue("{outfile}_rmse_logscale.pdf"), plog)
+
+}
